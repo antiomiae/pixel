21d4f26e8229e0b64603c5fbb65d34acaf9436f4
Kevin Ward <antiomiae@gmail.com>
2017-12-23 14:07:13 -0800

    initial commit

---
+++ src/app/App.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Kevin Ward on 12/23/17.
+//
+
+#include "App.h"
---
+++ src/app/App.h
@@ -0,0 +1,16 @@
+//
+// Created by Kevin Ward on 12/23/17.
+//
+
+#ifndef GL_APP_H
+#define GL_APP_H
+
+#define GLFW_INCLUDE_GLCOREARB
+#include <GLFW/glfw3.h>
+
+class App {
+App();
+};
+
+
+#endif //GL_APP_H
