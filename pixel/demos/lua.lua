ea0d79869bdbd4f0ad4364268d9894b0a792dd76
Kevin Ward <antiomiae@gmail.com>
2018-02-23 00:44:52 -0800

    Bind some glm types

--- pixel/demos/lua.lua
+++ pixel/demos/lua.lua
@@ -1,13 +1,24 @@
---
+function ivec2(v)
+    return pixel.glm.ivec2.new(v[1], v[2])
+end
 
-debug = require('debug')
+function vec4(v)
+    return pixel.glm.vec4.new(v[1], v[2], v[3], v[4])
+end
 
-local app = pixel.App.create({
-    width = 320,
-    height = 240,
+local app = pixel.App.create {
+    width = 620,
+    height = 480,
     background_color = { 0.5, 0.5, 0.5, 1.0 },
-    pixel_scale = 1.0
-})
+    pixel_scale = 2.0
+}
+
+-- Not possible yet
+--local app = pixel.App.new {
+--    ivec2 { 320, 240 },
+--    vec4 { 0.5, 0.5, 0.5, 1.0 },
+--    1.0
+--}
 
 app:init(0)
 
--- pixel/pixel/lua/lua.h
+++ pixel/pixel/lua/lua.h
@@ -12,20 +12,21 @@ namespace pixel
 
 
 sol::table bind_pixel(sol::state& lua);
-void bind_app(sol::table& lua, string type_name = "App");
-
+void bind_app(sol::state& lua, sol::table& binding, string type_name = "App");
+void bind_glm(sol::state& lua, sol::table& binding, string module_name = "glm");
 
 sol::table bind_pixel(sol::state& lua)
 {
     sol::table binding = lua["pixel"] = lua.create_table();
 
-    bind_app(binding);
+    bind_app(lua, binding);
+    bind_glm(lua, binding);
 
     return binding;
 }
 
 
-void bind_app(sol::table& lua, string type_name)
+void bind_app(sol::state& lua, sol::table& binding, string type_name)
 {
 
     auto app_from_table = [](sol::table args) -> unique_ptr<App> {
@@ -44,14 +45,29 @@ void bind_app(sol::table& lua, string type_name)
         return std::make_unique<App>(glm::ivec2{w, h}, bg_color, pixel_scale);
     };
 
-    auto type = lua.new_usertype<App>(
+    binding.new_usertype<App>(
         type_name,
-        "new", sol::constructors<App(), App(glm::ivec2, glm::vec4, float)>(),
+        "new", sol::constructors<App::App(void), App::App(glm::ivec2, glm::vec4, float)>(),
         "init", &App::init,
         "run", &App::run,
         "set_tick_callback", &App::set_tick_callback,
         "render_context", &App::render_context,
         "create", app_from_table
+    );
+}
+
+void bind_glm(sol::state& lua, sol::table& binding, string module_name)
+{
+    sol::table _glm = binding[module_name] = lua.create_table();
+
+    _glm.new_usertype<glm::ivec2>(
+        "ivec2",
+        "new", sol::constructors<glm::ivec2(), glm::ivec2(int, int)>()
+    );
+
+    _glm.new_usertype<glm::vec4>(
+        "vec4",
+        "new", sol::constructors<glm::vec4(void), glm::vec4(float, float, float, float)>()
     );
 }
 
