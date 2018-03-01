323c3ce583ac68435dbcde2eea06bbed45e3629b
Kevin Ward <antiomiae@gmail.com>
2018-02-28 16:54:46 -0800

    .

--- pixel/pixel/graphics/texture_atlas.cpp
+++ pixel/pixel/graphics/texture_atlas.cpp
@@ -9,6 +9,8 @@ void TextureAtlas::start_batch()
 {
     blocks_.clear();
     layers_.clear();
+    file_id_map_.clear();
+    top_id_ = 0;
 }
 
 
@@ -29,7 +31,6 @@ void TextureAtlas::stop_batch()
 
     sort(begin(packing), end(packing), [&](const auto& a, const auto& b) { return get<1>(a).z < get<1>(b).z; });
 
-
     ImageData temp_surface(texture_dimensions_.x, texture_dimensions_.y);
 
     auto current_layer = 0u;
@@ -38,15 +39,19 @@ void TextureAtlas::stop_batch()
         auto& image_size = get<0>(pack_pair);
         auto& pack_params = get<1>(pack_pair);
 
+        /* Encode position in atlas as TextureRegion */
         TextureRegion region;
+
         region.x = pack_params.x;
         region.y = pack_params.y;
         region.layer = pack_params.z;
         region.w = image_size.w;
         region.h = image_size.h;
 
+        /* Associate region with unique region ID */
         tex_regions_[image_size.region_id] = region;
 
+        /* Save current image as layer */
         if (current_layer != pack_params.z) {
             layers_.push_back(temp_surface);
             temp_surface.clear();
@@ -58,6 +63,7 @@ void TextureAtlas::stop_batch()
         current_layer = pack_params.z;
     }
 
+    /* Save last frame */
     layers_.push_back(temp_surface);
 
 //    blocks_.clear();
