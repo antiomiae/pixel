55c3a9513eac7ab79d7bf79421727c37760614a0
Kevin Ward <antiomiae@gmail.com>
2018-03-07 03:10:18 -0800

    Use atlas in lua demo

--- pixel/demos/lua.lua
+++ pixel/demos/lua.lua
@@ -18,17 +18,33 @@ app:init(0)
 
 local map = pixel.load_map("assets/map.tmx")
 
-local renderer = pixel.TileMapRenderer.new()
+local map_renderer = pixel.TileMapRenderer.new()
 
 local obs = {}
 table.insert(obs, map)
+
+local camera = pixel.Camera:new()
+
+local atlas = pixel.TextureAtlas.new(pixel.glm.uvec3.new(512, 512, 10))
+
+atlas:start_batch()
+atlas:add_image("assets/sonic.png")
+atlas:stop_batch()
+
+camera:set_window_size(320, 240)
+
+local j = 0.0
 
 app:set_tick_callback(function ()
     for i, o in pairs(obs) do
         o:update(1/60.0)
     end
 
-    renderer:render(map, app:render_context())
+    j = j + 0.01
+
+    camera:center_at(math.cos(j) * 50 + 320/2, math.sin(j) * 50 + 240 / 2)
+
+    map_renderer:render(map, camera)
 end)
 
 app:run()
