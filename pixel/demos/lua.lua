9e65dcbfadbcf0b885397df59495287b8524a359
Kevin Ward <antiomiae@gmail.com>
2018-03-18 17:31:18 -0700

    .

--- pixel/demos/lua.lua
+++ pixel/demos/lua.lua
@@ -1,13 +1,15 @@
 require 'pixel'
 
 local app = pixel.App.create {
-    width = 640,
-    height = 480,
+    width = 320,
+    height = 240,
     background_color = { 0.5, 0.5, 0.5, 1.0 },
     pixel_scale = 2.0
 }
 
 app:init(0)
+
+pixel.Keyboard.register_callback(app:window())
 
 -- Not possible yet
 --local app = pixel.App.new {
@@ -32,13 +34,43 @@ atlas:add_image("assets/sonic.png")
 atlas:stop_batch()
 
 camera:set_window_size(320, 240)
+camera:center_at(0, 0)
+--camera:position_at(0, 0)
 
 local j = 0.0
+
+local KEYS = {
+    W = string.byte('W'),
+    A = string.byte('A'),
+    S = string.byte('S'),
+    D = string.byte('D'),
+}
 
 app:set_tick_callback(function ()
     for i, o in pairs(obs) do
         o:update(1/60.0)
     end
+
+    j = j + 0.01
+
+    local tx = 0.0
+    local ty = 0.0
+
+    if pixel.Keyboard.keymap[KEYS.W] then
+        ty = 1.0
+    elseif pixel.Keyboard.keymap[KEYS.S] then
+        ty = -1.0
+    end
+
+    if pixel.Keyboard.keymap[KEYS.A] then
+        tx = -1.0
+    elseif pixel.Keyboard.keymap[KEYS.D] then
+        tx = 1.0
+    end
+
+    camera:translate(tx, ty)
+
+    camera:scale(0.2*math.sin(j) + 1.0)
 
     map_renderer:render(map, camera)
 end)
