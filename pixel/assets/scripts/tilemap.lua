2f73aa7f5fad1a3f822ddb3348d6a8bc798b6a0d
Kevin Ward <antiomiae@gmail.com>
2018-03-19 20:38:44 -0700

    .

--- pixel/assets/scripts/tilemap.lua
+++ pixel/assets/scripts/tilemap.lua
@@ -12,21 +12,30 @@ function TileMap:new(o)
     end
 end
 
+local function parse_layer(l)
+end
+
 function TileMap:__init(o)
     assert(o.tile_map, "requires pixel::TileMap argument tile_map")
 
     self.tile_map = o.tile_map
     self.tileset = self.tile_map:tileset()
+    self.layers = {}
 
     self.layer_count = #self.tile_map:layers()
-    assert(self.layer_count)
-
-    print(inspect(self))
+    for _, layer in ipairs(self.tile_map:layers()) do
+        table.insert(self.layers, parse_layer(layer))
+    end
 end
 
-function TileMap:get_tile_properties(x, y)
-    assert(x < self.tile_map:tile_count().x)
-    assert(x < self.tile_map:tile_count().y)
+function TileMap:get_tile_properties(x, y, layer_num)
+    local tile_count = self.tile_map:tile_count()
+
+    assert(x < tile_count.x and x >= 0)
+    assert(y < tile_count.y and y >= 0)
+
+    local layer = self.tile_map:layers()[layer_num]
+
 end
 
 return {
