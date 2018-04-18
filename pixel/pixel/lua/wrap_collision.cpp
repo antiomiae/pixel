c62de154ad67f1eb1e7764254e93031845d66b41
Kevin Ward <antiomiae@gmail.com>
2018-04-18 11:45:54 -0700

    .

--- pixel/pixel/lua/wrap_collision.cpp
+++ pixel/pixel/lua/wrap_collision.cpp
@@ -7,16 +7,6 @@ namespace pixel::binding
 using namespace std;
 using namespace pixel::collision;
 
-/*
- * CollisionMap() = default;
-    CollisionMap(uint width, uint height);
-
-    void set(int x, int y, bool solid);
-
-    bool collide_row(uint row, uint start_col, uint end_col);
-    bool collide_column(uint col, uint a, uint b);
- */
-
 void bind_collision_map(sol::state& lua, sol::table& binding, const string& type_name)
 {
     auto collison_map = binding.new_usertype<CollisionMap>(
