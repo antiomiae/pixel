5a60dd08fce149326a47882d5670010f81db2acc
Kevin Ward <antiomiae@gmail.com>
2018-06-03 14:57:38 -0700

    .

--- pixel/demos/tetrominos.cpp
+++ pixel/demos/tetrominos.cpp
@@ -93,72 +93,83 @@ private:
     vector<vector<Board::Piece>> pieces_;
 };
 
-
 class Level
 {
 public:
-    void tick();
-    void render();
+    void update()
+    {
+
+    }
+
+    void render()
+    {
+
+    }
+
 private:
     pixel::graphics::SpriteBatch sprite_batch_;
     pixel::graphics::renderers::SpriteRenderer sprite_renderer_;
     pixel::graphics::TextureAtlas texture_atlas_;
+
+    Board board_;
+    Tetromino current_piece_;
 };
 
 
 }
 
 
-
-
 int main(int argc, char* argv[])
 {
-//    if (argc >= 2) {
-//        std::cout << "Changing to directory " << argv[1] << std::endl;
-//        chdir(argv[1]);
-//        argv = &argv[2];
-//        argc -= 2;
-//    }
-//
-//    glm::ivec2 virtual_window_size = {320, 224};
-//    glm::ivec2 actual_window_size = virtual_window_size * 3;
-//
-//    pixel::init(actual_window_size, virtual_window_size);
-//
-//    Camera camera({0, 0}, {0, 0, 2000, 2000});
-//
-//    OffscreenRenderTarget render_target{};
-//
-//    render_target.set_window_size(virtual_window_size);
-//    camera.set_window_size(render_target.window_size());
-//
-//    pixel::app().set_tick_callback(
-//        [&] {
-//            render_target.activate();
-//
-//            glClear(GL_COLOR_BUFFER_BIT);
-//
-//            /* do rendering */
-//
-//            /* end */
-//
-//            render_target.deactivate();
-//
-//            glClear(GL_COLOR_BUFFER_BIT);
-//
-//            /* blit virtual window to actual window */
-//            render_target.draw(
-//                glm::ivec4(
-//                    0,
-//                    0,
-//                    app().render_context().window_size.x,
-//                    app().render_context().window_size.y
-//                )
-//            );
-//        }
-//    );
-//
-//    pixel::app().run();
+    if (argc >= 2) {
+        std::cout << "Changing to directory " << argv[1] << std::endl;
+        chdir(argv[1]);
+        argv = &argv[2];
+        argc -= 2;
+    }
+
+    glm::ivec2 virtual_window_size = {320, 224};
+    glm::ivec2 actual_window_size = virtual_window_size * 3;
+
+    pixel::init(actual_window_size, virtual_window_size);
+
+    Camera camera({0, 0}, {0, 0, 2000, 2000});
+
+    OffscreenRenderTarget render_target{};
+
+    render_target.set_window_size(virtual_window_size);
+    camera.set_window_size(render_target.window_size());
+
+    tetrominos::Level level{};
+
+    pixel::app().set_tick_callback(
+        [&] {
+            level.update();
+
+            render_target.activate();
+
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            level.render();
+
+            render_target.deactivate();
+
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            auto rc = app().render_context();
+            /* blit virtual window to actual window */
+            render_target.draw(
+                glm::ivec4(
+                    0,
+                    0,
+                    rc.window_size.x,
+                    rc.window_size.y
+                )
+            );
+        }
+    );
+
+    pixel::app().run();
 
     return 0;
 }
