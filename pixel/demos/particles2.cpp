23817579400329b6bec3f07265179f538efb1527
Kevin Ward <antiomiae@gmail.com>
2018-06-18 21:40:40 -0700

    Move particle code to library

--- pixel/demos/particles2.cpp
+++ pixel/demos/particles2.cpp
@@ -1,111 +1,17 @@
 #include <pixel/pixel.h>
+#include <pixel/physics/physics.h>
 #include <unistd.h>
-#include <random>
 #include <algorithm>
 #include <glm/gtx/component_wise.hpp>
-#include <glm/gtc/random.hpp>
 
 using namespace std;
 using namespace pixel;
 using namespace pixel::graphics;
+using namespace pixel::physics;
 using namespace pixel::input;
 
 const float DT = 1.0 / 60.0f;
 
-
-struct VelocityVerletParticle
-{
-
-    glm::vec2 position;
-    glm::vec2 last_position;
-    glm::vec2 velocity;
-    glm::vec2 acc;
-    float mass{1.0f};
-
-    glm::vec4 color{1.0, 1.0, 1.0, 1.0};
-
-    VelocityVerletParticle() = default;
-
-    VelocityVerletParticle(glm::vec2 position, glm::vec2 velocity = {0.0, 0.0}, float mass = 1.0,
-                           glm::vec4 color = {1.0, 1.0, 1.0, 1.0})
-        : position(position),
-          velocity(velocity),
-          mass(mass),
-          color(color)
-    {
-    }
-
-    void init_last_position(float dt)
-    {
-        last_position = position - dt * velocity;
-    }
-
-    static VelocityVerletParticle
-    random_particle(glm::vec4 pos_range, glm::vec2 vel_range, glm::vec2 mass_range)
-    {
-        static default_random_engine generator;
-
-        using random_float = uniform_real_distribution<float>;
-
-        random_float x_func(pos_range.x, pos_range.z);
-        random_float y_func(pos_range.y, pos_range.y);
-        random_float v_func(vel_range.x, vel_range.y);
-        random_float mass_func(mass_range.x, mass_range.y);
-
-        VelocityVerletParticle p{
-            glm::vec2(x_func(generator), y_func(generator)),
-            glm::circularRand(1.0f) * v_func(generator),
-            mass_func(generator)
-        };
-
-        return p;
-    }
-
-
-};
-
-
-struct IntegrationMethods
-{
-    template<class Particle, class AccFunc>
-    static void verlet_step(Particle& particle, AccFunc& acc, float dt)
-    {
-        auto next_position = (particle.position * 2.0f) - particle.last_position + (dt * dt) * acc(particle);
-        particle.last_position = particle.position;
-        particle.position = next_position;
-    }
-
-
-    template<class Particle, class AccFunc>
-    static void velocity_verlet_step(Particle& particle, AccFunc& acc, float dt)
-    {
-        auto next_position = particle.position + particle.velocity * dt + 0.5f * (dt * dt) * particle.acc;
-        particle.last_position = particle.position;
-        particle.position = next_position;
-
-        auto next_acc = acc(particle);
-        particle.velocity = particle.velocity + 0.5f * (particle.acc + next_acc) * dt;
-        particle.acc = next_acc;
-    }
-
-
-    template<class Particle, class AccFunc>
-    static void euler_cromer_step(Particle& particle, AccFunc& acc, float dt)
-    {
-        particle.velocity = particle.velocity + acc(particle) * dt;
-        particle.last_position = particle.position;
-        particle.position = particle.position + particle.velocity * dt;
-    }
-
-    template<class Particle, class AccFunc>
-    static void euler_step(Particle& particle, AccFunc& acc, float dt)
-    {
-        particle.last_position = particle.position;
-        particle.position = particle.position + particle.velocity * dt;
-        particle.velocity = particle.velocity + acc(particle) * dt;
-    }
-
-};
 
 
 template<class Particle>
