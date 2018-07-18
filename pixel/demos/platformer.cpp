60763ebf78ba0332127611dca91ee768f91d2fd8
Kevin Ward <antiomiae@gmail.com>
2018-07-18 07:40:41 -0700

    .

--- pixel/demos/platformer.cpp
+++ pixel/demos/platformer.cpp
@@ -7,6 +7,8 @@
 #include <glm/gtx/component_wise.hpp>
 #include <glm/gtx/color_space.hpp>
 #include <glm/gtx/fast_square_root.hpp>
+#include <glm/gtx/string_cast.hpp>
+
 
 using namespace std;
 using namespace pixel;
@@ -80,28 +82,43 @@ struct TileMapCollider
          */
 
         float acc_t = 0;
+        glm::ivec2 collision_index = {-1, -1};
 
         while (delta != glm::vec2{0, 0}) {
             float ct = std::numeric_limits<float>::infinity();
             bool check_column = false;
             bool check_row = false;
 
-            glm::ivec2 collision_index = {0, 0};
-
             if (delta.x != 0) {
+
                 float edge = test_rect.position.x + (dir.x == 1 ? test_rect.size.x : 0);
-                collision_index.x = (int) edge / (int) tile_size.x + (dir.x == 1 ? 1 : 0);
+                int idx = (int) (dir.x > 0 ? ceil(edge) : floor(edge)) / (int) tile_size.x + (dir.x == 1 ? 1 : 0);
+
+                // we've already checked this gridline and decided to go through it
+                if (collision_index.x == idx) {
+                    collision_index.x += dir.x;
+                } else {
+                    collision_index.x = idx;
+                }
 
                 // ensure we're going to collide with the map
                 if (collision_index.x >= (dir.x == 1 ? 0 : 1) && collision_index.x <= (dir.x == 1 ? tile_count.x - 1 : tile_count.x)) {
-                    ct = abs((collision_index.x * tile_size.x - (dir.x == 1 ? 1 : 0) - edge) * delta_inv.x);
+                    float grid_x = collision_index.x * tile_size.x - (dir.x == 1 ? 1 : 0);
+                    float d = abs(grid_x - edge);
+                    ct = d * delta_inv.x;
                     check_column = true;
                 }
             }
 
             if (delta.y != 0) {
                 float edge = test_rect.position.y + (dir.y == 1 ? test_rect.size.y: 0);
-                collision_index.y = (int) edge / (int) tile_size.y + (dir.y == 1 ? 1 : 0);
+                int idx = (int) edge / (int) tile_size.y + (dir.y == 1 ? 1 : 0);
+
+                if (collision_index.y == idx) {
+                    collision_index.y += dir.y;
+                } else {
+                    collision_index.y = idx;
+                }
 
                 // ensure we're going to collide with the map
                 if (collision_index.y >= (dir.y == 1 ? 0 : 1) && collision_index.y <= (dir.y == 1 ? tile_count.y - 1 : tile_count.y)) {
@@ -120,13 +137,18 @@ struct TileMapCollider
 
             // if ct >= 1, consume remaining delta and return
             if (ct > 1) {
+                cout << "ct > 1, delta = " << glm::to_string(delta) << endl;
                 test_rect.position += delta;
                 delta = {0, 0};
                 break;
             }
 
-            auto column = collision_index.x - (dir.x == -1 ? 1 : 0);
-            auto row = collision_index.y - (dir.y == -1 ? 1 : 0);
+            if (ct <= 1) {
+                int k = 10;
+            }
+
+            auto column = collision_index.x - (dir.x < 0 ? 1 : 0);
+            auto row = collision_index.y - (dir.y < 0 ? 1 : 0);
 
             auto d = ct * delta;
 
@@ -154,29 +176,31 @@ struct TileMapCollider
             delta_inv = 1.0f / delta;
 
             if (check_column) {
-                glm::ivec2 row_range = {
+                glm::ivec2 y_span = {
+                    clamp(test_rect.position.y, 0, tile_count.x * tile_size.x),
+                    clamp(test_rect.position.y + test_rect.size.y, 0, tile_count.x * tile_size.x)
+                };
+
+                glm::uvec2 row_range = {
                     max((int) test_rect.position.y / tile_size.y, 0),
                     min((int) (test_rect.position.y + test_rect.size.y) / tile_size.y, tile_count.y - 1)
                 };
 
                 error_if(column < 0, "column index less than 0");
 
-                for (auto y = row_range.s; y <= row_range.t; ++y) {
-                    auto& tile = tile_layer.at(column, y);
+                if (row_range != glm::uvec2{0, 0}) {
+                    for (auto y = row_range.s; y <= row_range.t; ++y) {
+                        auto& tile = tile_layer.at(column, y);
 
-                    if (tile.tile_id != 0) {
-                        auto& tile_desc = parent.tileset().tile(tile.tile_id);
+                        if (tile.tile_id != 0) {
+                            auto& tile_desc = parent.tileset().tile(tile.tile_id);
 
-                        if(tile_callback(tile, tile_desc)) {
-                            delta = {0, 0};
-                            break;
+                            if (tile_callback(tile, tile_desc)) {
+                                delta = {0, 0};
+                                break;
+                            }
                         }
                     }
-                }
-
-                if (ct == 0) {
-                    delta.x -= dir.x;
-                    test_rect.position.x += dir.x;
                 }
             }
 
@@ -188,24 +212,18 @@ struct TileMapCollider
 
                 error_if(row < 0, "row index less than 0");
 
-                for (auto x = col_range.s; x <= col_range.t; ++x) {
-                    auto& tile = tile_layer.at(x, row);
+                if (col_range != glm::uvec2{0, 0}) {
+                    for (auto x = col_range.s; x <= col_range.t; ++x) {
+                        auto& tile = tile_layer.at(x, row);
 
-                    if (tile.tile_id != 0) {
-                        auto& tile_desc = parent.tileset().tile(tile.tile_id);
+                        if (tile.tile_id != 0) {
+                            auto& tile_desc = parent.tileset().tile(tile.tile_id);
 
-                        if (tile_callback(tile, tile_desc)) {
-                            delta = {0, 0};
-                            break;
+                            if (tile_callback(tile, tile_desc)) {
+                                delta = {0, 0};
+                                break;
+                            }
                         }
-                    }
-
-                }
-
-                if (ct == 0) {
-                    if (abs(delta.y) >= 1) {
-                        delta.y -= dir.y;
-                        test_rect.position.y += dir.y;
                     }
                 }
             }
@@ -338,7 +356,14 @@ void run(Level& level)
 
     pixel::app().set_tick_callback(
         [&] {
-            guy.update(1 / 60.0f);
+            static bool flip = false;
+
+            //if (input::Keyboard::keymap[GLFW_KEY_SPACE] && !flip) {
+                guy.update(1 / 60.0f);
+//                flip = true;
+//            } else if (!input::Keyboard::keymap[GLFW_KEY_SPACE] && flip) {
+//                flip = false;
+//            }
 
             level.begin_render();
 
