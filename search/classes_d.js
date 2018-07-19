2692d246858b0772fb8f0ab8a0c028288fd9f33b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-07-19 03:57:16 +0000

    Deploy antiomiae/pixel to github.com/antiomiae/pixel.git:gh-pages

--- search/classes_d.js
+++ search/classes_d.js
@@ -7,6 +7,7 @@ var searchData=
   ['tile',['Tile',['../structpixel_1_1_tileset_1_1_tile.html',1,'pixel::Tileset::Tile'],['../structpixel_1_1_tile_layer_1_1_tile.html',1,'pixel::TileLayer::Tile']]],
   ['tileanimation',['TileAnimation',['../classpixel_1_1_tile_layer_1_1_tile_animation.html',1,'pixel::TileLayer']]],
   ['tileatlas',['TileAtlas',['../classpixel_1_1_tile_atlas.html',1,'pixel']]],
+  ['tilecoordinate',['TileCoordinate',['../struct_tile_map_collider_1_1_tile_coordinate.html',1,'TileMapCollider']]],
   ['tilelayer',['TileLayer',['../classpixel_1_1_tile_layer.html',1,'pixel']]],
   ['tilelayertexture',['TileLayerTexture',['../classpixel_1_1_tile_layer_texture.html',1,'pixel']]],
   ['tilemap',['TileMap',['../classpixel_1_1_tile_map.html',1,'pixel']]],
