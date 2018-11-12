5135c0b0aeeb5a8ce638a20765fe34ed27cd3ef5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-12 11:55:12 +0000

    Deploy antiomiae/pixel to github.com/antiomiae/pixel.git:gh-pages

--- search/all_1a.js
+++ search/all_1a.js
@@ -7,7 +7,6 @@ var searchData=
   ['_7elinerenderer',['~LineRenderer',['../classpixel_1_1renderers_1_1_line_renderer.html#afe637517d8e07a8524d855c2d002c08d',1,'pixel::renderers::LineRenderer']]],
   ['_7epacknode',['~PackNode',['../structpixel_1_1pack_1_1_pack_node.html#a8ce9fffbc415ea9509106959c99421fc',1,'pixel::pack::PackNode']]],
   ['_7erenderer',['~Renderer',['../classpixel_1_1renderers_1_1_renderer.html#a5828f2e137a86e99c1ce88f93421a9c7',1,'pixel::renderers::Renderer']]],
-  ['_7espriterenderer',['~SpriteRenderer',['../classpixel_1_1renderers_1_1_sprite_renderer.html#af3cca9fe3315ae2bf887e1a2cac25b65',1,'pixel::renderers::SpriteRenderer']]],
   ['_7etexture',['~Texture',['../classpixel_1_1graphics_1_1_texture.html#ae5214ca735ca5a0f1366f062aacf56d3',1,'pixel::graphics::Texture']]],
   ['_7etilemaprenderer',['~TileMapRenderer',['../classpixel_1_1renderers_1_1_tile_map_renderer.html#ad3f8dc87ae78ad82e3a278d1e1b2ea91',1,'pixel::renderers::TileMapRenderer']]]
 ];
