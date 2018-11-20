324e43db2d685779a617353131682b1c04d18077
Kevin Ward <antiomiae@gmail.com>
2018-11-19 21:34:39 -0800

    Manage glm flags in header

--- pixel/pixel/math.h
+++ pixel/pixel/math.h
@@ -5,7 +5,13 @@
 #define MAIN_MATH_H
 
 #include <cmath>
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmacro-redefined"
+#define GLM_MESSAGES 1
+#define GLM_ENABLE_EXPERIMENTAL
 #include <glm/glm.hpp>
+#pragma clang diagnostic pop
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtx/matrix_transform_2d.hpp>
