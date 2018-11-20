00b86d03ca4b0ba5a614a9abaf8630c6e4a0d124
Kevin Ward <antiomiae@gmail.com>
2018-11-19 21:36:43 -0800

    Finish implementing add_sprite

--- pixel/demos/textured_triangle_renderer.cpp
+++ pixel/demos/textured_triangle_renderer.cpp
@@ -17,6 +17,8 @@ class SpriteRenderer2
 {
 public:
     using Triangle = TexturedTriangleRenderer::Triangle;
+    using Vertex = TexturedTriangleRenderer::Vertex;
+
 
     explicit SpriteRenderer2(Level& level)
         :
@@ -32,8 +34,9 @@ public:
         }
 
         auto texture_region = sprite.texture_region;
+        auto size = glm::vec2(texture_region.w, texture_region.h);
         auto half_size = glm::vec2(texture_region.w, texture_region.h) / 2.0f;
-        auto center = glm::vec3(sprite.position + half_size * sprite.center, 0);
+        auto center = glm::vec3(sprite.position + size * (-0.5f - sprite.center), 0);
         // fill corners with w, h centered quad, rotate by angle
         // add center to each corner, build triangles
         array<glm::vec3, 4> corners = {
@@ -43,6 +46,7 @@ public:
             glm::vec3{half_size.x, half_size.y, 0}  // TR
         };
 
+
         auto rotate_matrix = glm::rotate(glm::mat3(), sprite.angle);
 
         std::transform(
@@ -50,8 +54,54 @@ public:
             [&](auto v) { return rotate_matrix * v + center; }
         );
 
-        tris_.push_back(Triangle(corners[0], corners[1], corners[2]));
-        tris_.push_back(Triangle(corners[1], corners[2], corners[3]));
+        auto layer = sprite.texture_region.layer;
+
+        array<glm::vec3, 4> texture_corners = {
+            glm::vec3{0, 0, layer},
+            glm::vec3{1, 0, layer},
+            glm::vec3{0, 1, layer},
+            glm::vec3{1, 1, layer}
+        };
+
+        std::for_each(
+            begin(texture_corners), end(texture_corners), [&](auto& v) {
+                if (sprite.flip_flags & Sprite::kHorizontal) {
+                    v.x = 1.0f - v.x;
+                }
+                if (sprite.flip_flags & Sprite::kVertical) {
+                    v.y = 1.0f - v.y;
+                }
+                if (sprite.flip_flags & Sprite::kDiagonal) {
+                    auto t = v;
+                    v.x = t.y * size.y;
+                    v.y = t.x * size.x;
+                }
+
+                v *= glm::vec3(size, 1.0f);
+
+                if (sprite.texture_region.flipped) {
+                    auto t = v;
+                    v.x = t.y;
+                    v.y = t.x;
+                }
+            }
+        );
+
+        tris_.push_back(
+            Triangle(
+                Vertex(corners[0], texture_corners[0], sprite.color),
+                Vertex(corners[1], texture_corners[1], sprite.color),
+                Vertex(corners[2], texture_corners[2], sprite.color)
+            )
+        );
+
+        tris_.push_back(
+            Triangle(
+                Vertex(corners[2], texture_corners[2], sprite.color),
+                Vertex(corners[1], texture_corners[1], sprite.color),
+                Vertex(corners[3], texture_corners[3], sprite.color)
+            )
+        );
     }
 
 private:
