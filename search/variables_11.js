bd686b56917906f19d78329498ff6e4cb6e125d5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-26 17:08:06 +0000

    Deploy antiomiae/pixel to github.com/antiomiae/pixel.git:gh-pages

--- search/variables_11.js
+++ search/variables_11.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['scale',['scale',['../classpixel_1_1graphics_1_1_sprite.html#a7b22539a44a0f40da782b2e3085d98e6',1,'pixel::graphics::Sprite']]],
   ['scale_5f',['scale_',['../classpixel_1_1graphics_1_1_camera.html#aec7c23c63fe67de6d66f3fc9622593e9',1,'pixel::graphics::Camera']]],
   ['shared_5fapp',['shared_app',['../classpixel_1_1_app.html#a6c73dfe2d542f1a9b526fbe6ad690057',1,'pixel::App']]],
   ['size',['size',['../struct_layout.html#a143bf56c3c9785cc75e2360dbe818e84',1,'Layout::size()'],['../structpixel_1_1graphics_1_1_attribute.html#a7657461b957ab3c74d3a6dc237f1581d',1,'pixel::graphics::Attribute::size()'],['../structpixel_1_1_bounding_box.html#a88f73dc3def621e23fb69f6dbf123f52',1,'pixel::BoundingBox::size()']]],
