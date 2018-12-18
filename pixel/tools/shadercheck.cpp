effdd9ac3b13565dc05aad9c860196b62eda95d4
Kevin Ward <antiomiae@gmail.com>
2018-12-17 19:28:20 -0800

    Fix

--- pixel/tools/shadercheck.cpp
+++ pixel/tools/shadercheck.cpp
@@ -11,16 +11,18 @@ using pixel::App;
 using pixel::graphics::Shader;
 
 int main(int argc, char *argv[]) {
+    pixel::init();
+
     if (argc < 3) {
         cerr << "ERROR: vertex and fragment shader paths required" << endl;
         cerr << "Usage: shadercheck VS_PATH FS_PATH" << endl;
         exit(1);
     }
 
-    App app;
-    app.init();
+    glfwHideWindow(pixel::app().window());
 
-    glfwHideWindow(app.window());
+    cout << "vs path: " << argv[1] << endl;
+    cout << "fs path: " << argv[2] << endl;
 
     Shader shader(argv[1], argv[2]);
 
