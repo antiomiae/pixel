916bcf7485454704f5af36479a3215316af55a5a
Kevin Ward <antiomiae@gmail.com>
2018-12-25 14:27:19 -0800

    Initialize sample image data to pure white

--- pixel/test/graphics/test_texture_atlas.cpp
+++ pixel/test/graphics/test_texture_atlas.cpp
@@ -9,28 +9,21 @@ using namespace pixel::graphics;
 
 TEST_CASE("TextureAtlas")
 {
-    TextureAtlas atlas({1024, 1024, 20});
+    TextureAtlas atlas({512, 512, 20});
 
     atlas.start_batch();
 
-    for (auto i = 0u; i < 250; ++i) {
+    for (auto i = 0u; i < 50; ++i) {
         atlas.add_image("assets/random_images/" + to_string(i) + ".png");
     }
 
     ImageData img{32, 32};
+    memset(img.data, 255, img.width * img.height * img.bpp);
     atlas.add_image(img, "ImageData image");
 
     atlas.stop_batch();
 
     auto texture = atlas.as_texture();
-
-    int i = 0;
-    for (auto& img : atlas.layers()) {
-        img.save("atlas_"s + to_string(i) + ".png"s);
-        i++;
-    }
-
-
 }
 
 };
